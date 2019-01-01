7dae3363db7f547443708196c372c3355ee8036a
Aiyush Jain <jain.aiyush@fitster.ca>
2019-01-01 15:11:08 -0500

    implemented initial databinding for components - updated angular 6 --> angular 7

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe("Test Recipe", "This is a test.", "http://fitster.ca/assets/img/work_placeholder_1.jpg")
+    new Recipe("Test Recipe", "This is a test.", "http://fitster.ca/assets/img/work_placeholder_1.jpg"),
+    new Recipe("Another Test Recipe", "This is a test.", "http://fitster.ca/assets/img/work_placeholder_1.jpg")
   ];
 
   constructor() { }
@@ -16,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
