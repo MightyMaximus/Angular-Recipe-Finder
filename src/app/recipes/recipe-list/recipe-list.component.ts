3546cfa7301b44c258fb76ff7e58cabf1a67e0f4
Aiyush Jain <jain.aiyush@fitster.ca>
2019-01-01 03:46:20 -0500

    set up basic app structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "This is a test.", "http://fitster.ca/assets/img/work_placeholder_1.jpg")
+  ];
 
   constructor() { }
 
